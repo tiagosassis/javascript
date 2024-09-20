364737e7c6822d353b138bb87d80208d89229619
Tiago Silva de Assis <tiaguiassis@gmail.com>
2024-09-20 17:30:58 -0300

    update

--- ex009/scripts/script.js
+++ ex009/scripts/script.js
@@ -1,7 +1,17 @@
 let button = document.querySelectorAll('div')
 let display = document.getElementById('display')
 buttons.addEventListener('click', (e)=>{
-    display.innerHTML += e.target.innerText
+    if (e.target.innerText == 'C') {
+        display.innerText = ''
+    } else if(e.target.innerText == 'backspace'){
+        display.innerText = display.innerText.substring(0, display.innerText.length - 1)
+    }
+    else{
+        display.innerHTML += e.target.innerText
+    }
+    console.log(e.currentTarget)
 })
 
 
+// animationTarget.style.animation = "";
+// setTimeout(() => animationTarget.style.animation = "ResizeButtonAnimation .1s linear");
